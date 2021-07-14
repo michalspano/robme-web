10db9301f322959c44ab70efe0be91a3826c74da
michalspano <speko.spano@gmail.com>
2021-07-14 20:56:01 +0200

    Update (Checkboxes and arrays)✅

---
+++ script.ts
@@ -0,0 +1,15 @@
+// Typescript file to be used
+// Named fucntion
+function add(x, y)
+{
+    return x + y;
+};
+// Anonymous function
+let myAdd = function (x, y)
+{
+    return x + y;
+};
+
+var result = myAdd(10, 10);
+console.log("Function result: " + result);
+// alert(result);
