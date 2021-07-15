e5f149122e7e3f0b4dd0b70e60b926699c21b3bc
michalspano <speko.spano@gmail.com>
2021-07-15 10:45:56 +0200

    Update ✅

--- site2.php
+++ site2.php
@@ -1,11 +1,28 @@
 <!DOCTYPE html>
 <htmt lang="en">
     <head>
-        <link>
+        <link rel="stylesheet" href="style2.css">
         <title> This is a secondary website </title>
     </head>
     <body>
-        <h1> This is a secondary website! </h1>
+    <h1> Dictionaries in php! </h1>
+    <form action="site2.php" method="post">
+        Input a student's name: <input type="text" name="student">
+        <input type="submit">
+    </form>
+    <?php
+        // Associative arrays / Dictionaries;
+        // "key": value; ["key"=>value];
+        $grades = ["Michal"=>"A+", "Daniel"=>"A", "Peter"=>"B", "Fares"=>"A-"];
+        $desired_student_name = $_POST["student"];
+        echo "$desired_student_name's grade: ";
+        echo "<b><i>$grades[$desired_student_name]</i></b>";
+
+        for ($r = 0; $r < count($grades); $r++)
+        {
+            echo "<br>$r";
+        }
+    ?>
     </body>
     <script src="secondaryScript.ts"></script>
 </htmt>
