f5f9eca158bb659e213c2666b481754c95e8c464
michalspano <speko.spano@gmail.com>
2021-07-15 18:24:06 +0200

    Update (Math website) ✅

--- Math-project/mathCore.php
+++ Math-project/mathCore.php
@@ -1,7 +1,45 @@
 <html>
-    <head></head>
+    <head lang="en">
+        <title> Math website :) </title>
+    </head>
     <link rel="stylesheet" href="style.css">
     <body>
-        <?php include "header.html"; ?>
+        <?php include "header.html"; // Includes the local header file?>
+        <p> Possible math opeations: 1. power, 2. log, 3. factorial </p>
+        <form action="mathCore.php" method="post">
+            <p> Choose an operation: </p> <input type="number" name="operation">
+            <p> Value 1: </p> <input type="number" name="num1">
+            <p> Value 2: </p> <input type="number" name="num2">
+            <br><br> <input type="submit" name="Done!">
+        </form>
+        <?php
+            // Get the operator and the values from the user
+            $desiredMathOperation = $_POST["operation"];
+            $num1 = $_POST["num1"];
+            $num2 = $_POST["num2"];
+
+            // Refer to a dynamic (local) php file with math operations;
+            include "mathFunctions.php";
+
+            // Switch statement to decide on various opearions
+            switch ($desiredMathOperation)
+                // Uses math functions from a local php file
+            {
+                case 1:
+                    $output = powerFunction($num1, $num2);
+                    break;
+                case 2:
+                    $output = logFunction($num1, $num2);
+                    break;
+                case 3:
+                    $output = factorialFunction($num1);
+                    break;
+                default:
+                    // Handles the incorrect operator
+                    echo "Error! Missing operation";
+            }
+            // Exports the numeric result
+            echo "<p>Result: $output</p>";
+        ?>
     </body>
 </html>
--- Math-project/mathFunctions.php
+++ Math-project/mathFunctions.php
@@ -1,3 +1,19 @@
 <?php
-
+    function powerFunction($base, $exponent)
+    {
+        return pow($base, $exponent);
+    }
+    function logFunction($exponent, $base)
+    {
+        return log($exponent, $base);
+    }
+    function factorialFunction($exponent)
+    {
+        $output = 1;
+        for ($i = 2; $i <= $exponent; $i++)
+        {
+            $output *= $i;
+        }
+        return $output;
+    }
 ?>
