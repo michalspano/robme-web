2001b4bbe9428fbdb605bef6cd143166d2cdfcfa
michalspano <speko.spano@gmail.com>
2021-07-15 11:20:28 +0200

    Update (Math calculator)✅

--- site2.php
+++ site2.php
@@ -6,6 +6,7 @@
     </head>
     <body>
     <h1> Dictionaries in php! </h1>
+
     <form action="site2.php" method="post">
         Input a student's name: <input type="text" name="student">
         <input type="submit">
@@ -15,13 +16,40 @@
         // "key": value; ["key"=>value];
         $grades = ["Michal"=>"A+", "Daniel"=>"A", "Peter"=>"B", "Fares"=>"A-"];
         $desired_student_name = $_POST["student"];
+
         echo "$desired_student_name's grade: ";
         echo "<b><i>$grades[$desired_student_name]</i></b>";
+        echo "<hr>";
+    ?>
+    <h1> Functions in php! </h1>
+    <p> Making a simple calculator </p>
+    <form action="site2.php" method="post">
+        Number 1: <input type="number" name="num1">
+        Number 2: <input type="number" name="num2">
+        Operation: <input type="text" name="op">
+        <input type="submit">
+    </form>
+    Output: <?php
+        $num1 = $_POST["num1"];
+        $num2 = $_POST["num2"];
+        $operator = $_POST["op"];
 
-        for ($r = 0; $r < count($grades); $r++)
+        // Univeral math function
+        function mathFunctionHandler($n1, $n2, $op)
         {
-            echo "<br>$r";
+            switch ($op) {
+                case "+":
+                    return $n1 + $n2;
+                case "-":
+                    return $n1 + $n2;
+                case "*":
+                    return $n1 * $n2;
+                case "/":
+                    return $n1 / $n2;
+            }
         }
+        $result = round(mathFunctionHandler($num1, $num2, $operator), 2);
+        echo "<p> $num1 $operator $num2 = <b>$result</b></p>";
     ?>
     </body>
     <script src="secondaryScript.ts"></script>
