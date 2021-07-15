13657a19946e1d53e2e1e33ede47398e212db336
michalspano <speko.spano@gmail.com>
2021-07-15 10:22:07 +0200

    Update ✅

---
+++ site2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<htmt lang="en">
+    <head>
+        <link>
+        <title> This is a secondary website </title>
+    </head>
+    <body>
+        <h1> This is a secondary website! </h1>
+    </body>
+    <script src="secondaryScript.ts"></script>
+</htmt>
