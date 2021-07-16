44f05848024763b43d1a73798e70486bc9c03979
michalspano <speko.spano@gmail.com>
2021-07-16 10:20:05 +0200

    Update (Setters and Getters) ✅

--- dataClasses.php
+++ dataClasses.php
@@ -29,9 +29,90 @@
             $book1 = new Book("Book 1", "Harry Potter",
                 "JK Rowling", 400);  // Calling the Constructor with values
 
+            // Access the class information
+            $book1->pages = 500;
             echo "<p>$book1->title</p>";
             echo "<p>$book1->author</p>";
             echo "<p>$book1->pages</p>";
+            echo "<hr>";
+        ?>
+        <?php
+            class Student
+            {
+                var $name;
+                var $major;
+                var $gpa;
+
+                function __construct($name, $major, $gpa)
+                {
+                    $this->name = $name;
+                    $this->major = $major;
+                    $this->gpa = $gpa;
+                }
+
+                function hasHonors()
+                {
+                    if ($this->gpa >= 3.5)
+                    {
+                        return "true";
+                    }
+                    else
+                    {
+                        return "false";
+                    }
+                }
+            }
+            // Student instances;
+            $student1 = new Student("Michal", "Math", 4);
+            $student2 = new Student("Fares", "Inf", 2);
+            // Calls the method of a class;
+            echo $student1->hasHonors();
+            echo "<br>";
+            echo $student2->hasHonors();
+            echo "<hr>";
+        ?>
+        <?php
+            // Getters and setters;
+            class Movie
+            {
+                // Public or private key values;
+                // public = var - interchangable;
+                public $title;
+
+                // Only accessed within the class itself;
+                // Setters and getters and needed
+                private $rating;
+
+                function __construct($title, $rating)
+                {
+                    $this->title = $title;
+                    // Handles the specified condition;
+                    $this->setRating($rating);
+                }
+                // Access the rating even if private var;
+                // Getter function;
+                function getRating()
+                {
+                    return $this->rating;
+                }
+                // Modify the rating;
+                // Setter function;
+                function setRating($rating)
+                {
+                    // Only assign the rating value if this condition is completed;
+                    if ($rating == "G" || $rating == "PG" || $rating == "R")
+                    {
+                        $this->rating = $rating;
+                    }
+                    // Else assign "NR" -> Not Rated;
+                    else
+                    {
+                        $this->rating = "NR";
+                    }
+                }
+            }
+            $avengers = new Movie("Avengers", "Dog");
+            echo $avengers->getRating();
         ?>
     </body>
 </html>
