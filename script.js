607fd0bbd6b9c60cc74e12cbff24879fe5b9a22c
michalspano <speko.spano@gmail.com>
2021-08-03 11:32:05 +0200

    Add minor changes

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-console.log("Script console test");
-
-let greeUser = "Hello";
-for (let i = 0; i < 10; i++)
-{
-    console.log(greeUser);
-    console.log("\n");
+"use strict";
+// Typescript file to be used
+// Named fucntion
+function add(x, y) {
+    return x + y;
 }
+;
+// Anonymous function
+var myAdd = function (x, y) {
+    return x + y;
+};
+var result = myAdd(10, 10);
+console.log("Function result: " + result);
+// alert(result);
